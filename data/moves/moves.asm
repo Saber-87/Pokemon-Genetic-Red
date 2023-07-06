263fe77912b0ab457919a11b512933287940b608
Saber-87 <95220206+Saber-87@users.noreply.github.com>
2023-07-06 13:41:07 +0200

    Corrected Charm

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -175,6 +175,6 @@ Moves:
 	move SUPER_FANG,   SUPER_FANG_EFFECT,            1, NORMAL,        90, 10
 	move SLASH,        NO_ADDITIONAL_EFFECT,        70, NORMAL,       100, 20
 	move SUBSTITUTE,   SUBSTITUTE_EFFECT,            0, NORMAL,       100, 10
-	move LOVELY_KISS,  ATTACK_DOWN2_EFFECT,		 0, FAIRY,	  100, 20
+	move CHARM,  	   ATTACK_DOWN2_EFFECT,		 0, FAIRY,	  100, 20
 	move STRUGGLE,     RECOIL_EFFECT,               50, NORMAL,       100, 10
 	assert_table_length NUM_ATTACKS
